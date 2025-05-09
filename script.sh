a85d72adfef94ab4149faa280317434f0d42ad38
ervand7 <ervand7@mail.ru>
2025-05-09 21:43:02 +0200

    added sh script

---
+++ script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
+
+docker system prune -f
+
+docker compose up -d
+
+docker exec -it kafka kafka-topics.sh \
+  --create \
+  --topic orders \
+  --partitions 3 \
+  --replication-factor 1 \
+  --if-not-exists \
+  --bootstrap-server kafka:9092
+
+docker exec -it kafka kafka-topics.sh \
+  --create \
+  --topic orders-taxed \
+  --partitions 3 \
+  --replication-factor 1 \
+  --bootstrap-server kafka:9092
+
+docker compose up -d --scale consumer=2
