0cb755ee288a4e0410a3be623affe023aa73cd19
ervand7 <ervand7@mail.ru>
2025-05-10 14:33:34 +0200

    added retention logic

--- script.sh
+++ script.sh
@@ -2,9 +2,8 @@
 
 # shellcheck disable=SC2046
 docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
-
 docker system prune -f
-
+docker compose build --no-cache
 docker compose up -d
 
 docker exec -it kafka kafka-topics.sh \
@@ -13,21 +12,23 @@ docker exec -it kafka kafka-topics.sh \
   --partitions 3 \
   --replication-factor 1 \
   --if-not-exists \
-  --bootstrap-server kafka:9092
+  --bootstrap-server kafka:9092 \
+  --config retention.bytes=10485760
 
 docker exec -it kafka kafka-topics.sh \
   --create \
   --topic orders-taxed \
   --partitions 3 \
   --replication-factor 1 \
-  --bootstrap-server kafka:9092
+  --bootstrap-server kafka:9092 \
+  --config retention.bytes=10485760
 
 docker exec -it kafka kafka-topics.sh \
   --create \
   --topic orders-dlq \
   --partitions 3 \
   --replication-factor 1 \
-  --bootstrap-server kafka:9092
-
+  --bootstrap-server kafka:9092 \
+  --config retention.bytes=10485760
 
 docker compose up -d --scale consumer=2
